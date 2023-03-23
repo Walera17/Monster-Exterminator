6204de6e049b8fbac78e4a6a10fe84eac8274254
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-23 06:22:56 +0200

    06 Add Aiming Control

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,46 +5,62 @@ namespace MonsterExterminator.Player
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private JoyStick joyStick;
+        [SerializeField] private JoyStick moveStick;
+        [SerializeField] private JoyStick aimStick;
         [SerializeField] CharacterController characterController;
         [SerializeField] private CameraController cameraController;
         [SerializeField] private float moveSpeed = 20f;
         [SerializeField] private float turnSpeed = 30f;
 
-        private Vector2 moveInput;
+        private Vector2 moveInput, aimInput;
         private Camera mainCamera;
 
         void Start()
         {
             mainCamera = Camera.main;
-            joyStick.OnStickInputValueChanged += JoyStick_OnStickInputValueChanged;
+            moveStick.OnStickInputValueChanged += MoveStick_OnStickInputValueChanged;
+            aimStick.OnStickInputValueChanged += AimStick_OnStickInputValueChanged;
         }
 
         private void OnDestroy()
         {
-            joyStick.OnStickInputValueChanged -= JoyStick_OnStickInputValueChanged;
+            moveStick.OnStickInputValueChanged -= MoveStick_OnStickInputValueChanged;
+            aimStick.OnStickInputValueChanged -= AimStick_OnStickInputValueChanged;
         }
 
-        private void JoyStick_OnStickInputValueChanged(Vector2 value)
+        private void MoveStick_OnStickInputValueChanged(Vector2 value)
         {
             moveInput = value;
         }
 
+        private void AimStick_OnStickInputValueChanged(Vector2 value)
+        {
+            aimInput = value;
+        }
+
+        Vector3 StickInputToWorldDirection(Vector2 inputValue)
+        {
+            Vector3 rightDir = mainCamera.transform.right;                      // направление вправо персонажа
+            Vector3 upDir = Vector3.Cross(rightDir, Vector3.up);                // направление вперед персонажа
+            return rightDir * inputValue.x + upDir * inputValue.y;              // мировое направление движения персонажа
+        }
+
         void Update()
         {
-            Vector3 rightDir = mainCamera.transform.right; // направление вправо персонажа
-            Vector3 upDir = Vector3.Cross(rightDir, Vector3.up); // направление вперед персонажа
-            Vector3 moveDirection = rightDir * moveInput.x + upDir * moveInput.y; // направление движения персонажа
+            Vector3 moveDirection = StickInputToWorldDirection(moveInput);
+
+            if (aimInput.magnitude != 0)
+            {
+                Vector3 aimDirection = StickInputToWorldDirection(aimInput);
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDirection, Vector3.up), turnSpeed * Time.deltaTime);
+            }
+            else if (moveInput.magnitude != 0)
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection, Vector3.up), turnSpeed * Time.deltaTime);
 
             characterController.Move(moveDirection * (moveSpeed * Time.deltaTime));
 
-            if (moveInput.magnitude != 0)
-            {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(moveDirection, Vector3.up), turnSpeed * Time.deltaTime);
-
-                if (cameraController != null)
-                    cameraController.AddYawInput(moveInput.x);
-            }
+            if (moveInput.magnitude != 0 && cameraController != null)
+                cameraController.AddYawInput(moveInput.x);
         }
     }
 }
