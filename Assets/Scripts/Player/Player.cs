356fb8281a7a1890fe33fd15f54e9e109247cc73
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-23 04:08:01 +0200

    03 Create Player and using the Camera Orientation to Calculate Move Directions

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,38 @@
+using MonsterExterminator.UI;
+using UnityEngine;
+
+namespace MonsterExterminator.Player
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private JoyStick joyStick;
+        [SerializeField] CharacterController characterController;
+        [SerializeField] private float moveSpeed = 20;
+
+        private Vector2 moveInput;
+        private Camera mainCamera;
+
+        void Start()
+        {
+            mainCamera = Camera.main;
+            joyStick.OnStickInputValueChanged += JoyStick_OnStickInputValueChanged;
+        }
+
+        private void OnDestroy()
+        {
+            joyStick.OnStickInputValueChanged -= JoyStick_OnStickInputValueChanged;
+        }
+
+        private void JoyStick_OnStickInputValueChanged(Vector2 value)
+        {
+            moveInput = value;
+        }
+
+        void Update()
+        {
+            Vector3 rightDir = mainCamera.transform.right;                                                              // направление вправо персонажа
+            Vector3 upDir = Vector3.Cross(rightDir, Vector3.up);           	                                            // направление вперед персонажа
+            characterController.Move((rightDir * moveInput.x + upDir * moveInput.y) * (Time.deltaTime * moveSpeed));
+        }
+    }
+}
--- Assets/Scripts/UI/JoyStick.cs
+++ Assets/Scripts/UI/JoyStick.cs
@@ -9,25 +9,35 @@ namespace MonsterExterminator.UI
         [SerializeField] RectTransform backgroundTransform;
         [SerializeField] RectTransform centerTransform;
 
+        public delegate void OnStickInputValueUpdate(Vector2 value);
+
+        public event OnStickInputValueUpdate OnStickInputValueChanged;
+
         public void OnDrag(PointerEventData eventData)
         {
             Vector2 touchPosition = eventData.position;
             Vector2 centerPosition = backgroundTransform.position;
+
             Vector2 localOffset = Vector2.ClampMagnitude(touchPosition - centerPosition, backgroundTransform.sizeDelta.x / 2);
+            Vector2 inputValue = localOffset / backgroundTransform.sizeDelta.x / 2;
 
             stickTransform.position = centerPosition + localOffset;
+
+            OnStickInputValueChanged?.Invoke(inputValue);
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
             backgroundTransform.position = eventData.position;
-            stickTransform.position= eventData.position;
+            stickTransform.position = eventData.position;
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
             backgroundTransform.position = centerTransform.position;
             stickTransform.position = backgroundTransform.position;
+
+            OnStickInputValueChanged?.Invoke(Vector2.zero);
         }
     }
 }
