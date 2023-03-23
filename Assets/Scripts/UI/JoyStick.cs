e3ca71e5fcc0325a2943bbe7b7947938bddd300c
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-23 02:20:46 +0200

    02 Create JoyStick

--- Assets/Scripts/UI/JoyStick.cs
+++ Assets/Scripts/UI/JoyStick.cs
@@ -5,17 +5,29 @@ namespace MonsterExterminator.UI
 {
     public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
     {
+        [SerializeField] RectTransform stickTransform;
+        [SerializeField] RectTransform backgroundTransform;
+        [SerializeField] RectTransform centerTransform;
+
         public void OnDrag(PointerEventData eventData)
         {
-            print("OnDrag = " + eventData.position);
+            Vector2 touchPosition = eventData.position;
+            Vector2 centerPosition = backgroundTransform.position;
+            Vector2 localOffset = Vector2.ClampMagnitude(touchPosition - centerPosition, backgroundTransform.sizeDelta.x / 2);
+
+            stickTransform.position = centerPosition + localOffset;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
+            backgroundTransform.position = eventData.position;
+            stickTransform.position= eventData.position;
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
+            backgroundTransform.position = centerTransform.position;
+            stickTransform.position = backgroundTransform.position;
         }
     }
 }
