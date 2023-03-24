6b13bedc06d9a603b0811b125ca178ca433a7e41
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-24 03:52:01 +0200

    09 Create Player Animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,12 +8,18 @@ namespace MonsterExterminator.Player
         [SerializeField] private JoyStick moveStick;
         [SerializeField] private JoyStick aimStick;
         [SerializeField] CharacterController characterController;
+        [SerializeField] Animator animator;
         [SerializeField] private CameraController cameraController;
-        [SerializeField] private float moveSpeed = 20f;
-        [SerializeField] private float turnSpeed = 30f;
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float turnSpeed = 8f;
+        [SerializeField] private float animTurnSpeed = 5f;
 
         private Vector2 moveInput, aimInput;
         private Camera mainCamera;
+        private float animatorTurnSpeed;
+        private static readonly int ForwardSpeed = Animator.StringToHash("forwardSpeed");
+        private static readonly int RightSpeed = Animator.StringToHash("rightSpeed");
+        private static readonly int TurnSpeed = Animator.StringToHash("turnSpeed");
 
         void Start()
         {
@@ -40,9 +46,9 @@ namespace MonsterExterminator.Player
 
         Vector3 StickInputToWorldDirection(Vector2 inputValue)
         {
-            Vector3 rightDir = mainCamera.transform.right;                              // направление вправо персонажа
-            Vector3 upDir = Vector3.Cross(rightDir, Vector3.up);                        // направление вперед персонажа
-            return rightDir * inputValue.x + upDir * inputValue.y;                      // мировое направление движения персонажа
+            Vector3 rightDir = mainCamera.transform.right; // направление вправо персонажа
+            Vector3 upDir = Vector3.Cross(rightDir, Vector3.up); // направление вперед персонажа
+            return rightDir * inputValue.x + upDir * inputValue.y; // мировое направление движения персонажа
         }
 
         void Update()
@@ -58,6 +64,12 @@ namespace MonsterExterminator.Player
             characterController.Move(moveDirection * (moveSpeed * Time.deltaTime));
 
             UpdateAim(moveDirection);
+
+            float forward = Vector3.Dot(moveDirection, transform.forward);
+            float right = Vector3.Dot(moveDirection, transform.right);
+
+            animator.SetFloat(ForwardSpeed, forward);
+            animator.SetFloat(RightSpeed, right);
         }
 
         private void UpdateAim(Vector3 moveDirection)
@@ -78,8 +90,26 @@ namespace MonsterExterminator.Player
 
         private void RotateToward(Vector3 aimDirection)
         {
+            float currentTurnSpeed = 0;
+
             if (aimDirection.magnitude != 0)
+            {
+                Quaternion prevRot = transform.rotation;
+
                 transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(aimDirection, Vector3.up), turnSpeed * Time.deltaTime);
+
+                Quaternion currentRot = transform.rotation;
+                float dir = Vector3.Dot(aimDirection, transform.right) > 0 ? 1 : -1;
+                float rotationDelta = Quaternion.Angle(prevRot, currentRot) * dir;
+                currentTurnSpeed = rotationDelta / Time.deltaTime;
+            }
+
+            animatorTurnSpeed = Mathf.Lerp(animatorTurnSpeed, currentTurnSpeed, animTurnSpeed * Time.deltaTime);
+
+            if (Mathf.Abs(animatorTurnSpeed) < 0.01f)
+                animatorTurnSpeed = 0;
+
+            animator.SetFloat(TurnSpeed, animatorTurnSpeed);
         }
     }
 }
