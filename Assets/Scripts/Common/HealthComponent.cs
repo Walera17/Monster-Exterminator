9dc4a091afeb4c38bbf090fb439bb8bd913950df
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-26 07:00:22 +0300

    20 Create the Enemy Class and Animator

--- Assets/Scripts/Common/HealthComponent.cs
+++ Assets/Scripts/Common/HealthComponent.cs
@@ -8,18 +8,18 @@ namespace MonsterExterminator.Common
         public delegate void OnDeadDelegate();
 
         public event OnHealthChangeDelegate OnHealthChange, OnTakeDamage;
-        public event OnDeadDelegate  OnDead;
+        public event OnDeadDelegate OnDead;
 
         [SerializeField] float health = 100;
         [SerializeField] float maxHealth = 100;
 
         public void ChangeHealth(float delta)
         {
-            if (delta == 0) return;
+            if (delta == 0 || health == 0) return;
 
             health += delta;
 
-            if (delta < 0) 
+            if (delta < 0)
                 OnTakeDamage?.Invoke(health, delta, maxHealth);
 
             OnHealthChange?.Invoke(health, delta, maxHealth);
---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using MonsterExterminator.Common;
+using UnityEngine;
+
+namespace MonsterExterminator.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private HealthComponent healthComponent;
+        [SerializeField] Animator animator;
+
+        private static readonly int Dead = Animator.StringToHash("dead");
+        private static readonly int Hit = Animator.StringToHash("hit");
+
+        private void Start()
+        {
+            healthComponent.OnTakeDamage += HealthComponent_OnTakeDamage;
+            healthComponent.OnDead += HealthComponent_OnDead;
+        }
+
+        private void OnDestroy()
+        {
+            healthComponent.OnTakeDamage -= HealthComponent_OnTakeDamage;
+            healthComponent.OnDead -= HealthComponent_OnDead;
+        }
+
+        private void HealthComponent_OnDead()
+        {
+            TriggerDeathAnimation();
+        }
+
+        private void TriggerDeathAnimation()
+        {
+            animator.SetTrigger(Dead);
+        }
+
+        public void AnimatorDestroyGameObject()
+        {
+            Destroy(gameObject);
+        }
+
+        private void HealthComponent_OnTakeDamage(float health, float delta, float maxHealth)
+        {
+            animator.SetTrigger(Hit);
+        }
+    }
+}
