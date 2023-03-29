e2c0be08f5d9119cff0675365115349eab7ab8b8
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-03-29 04:00:06 +0300

    30 Basics of Compositors

---
+++ Assets/Scripts/Common/BehaviorTree/Compositor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace MonsterExterminator.Common.BehaviorTree
+{
+    public abstract class Compositor : Node
+    {
+        readonly LinkedList<Node> children = new();
+        LinkedListNode<Node> currentChild;
+
+        protected override NodeResult Execute()
+        {
+            if (children.Count == 0)
+            {
+                return NodeResult.Success;
+            }
+
+            currentChild = children.First;
+            return NodeResult.Inprogress;
+        }
+
+        protected bool Next()
+        {
+            if (currentChild != children.Last)
+            {
+                currentChild = currentChild.Next;
+                return true;
+            }
+
+            return false;
+        }
+
+        protected override void End()
+        {
+            currentChild = null;
+        }
+    }
+}
