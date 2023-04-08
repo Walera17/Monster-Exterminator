5f0874d8b6bcbb24866998eb2fb45925c297b7a7
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-04-09 00:21:43 +0300

    68 Basics of Ability and Ability Component

--- Assets/Scripts/AbilitySystem/AbilityComponent.cs
+++ Assets/Scripts/AbilitySystem/AbilityComponent.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-namespace Characters.AbilitySystem
+namespace AbilitySystem
 {
     public class AbilityComponent : MonoBehaviour
     {
         [SerializeField] Ability[] initialAbilities;
+        [SerializeField] private float stamina = 200f;
+        [SerializeField] private float maxStamina = 200f;
+
         readonly List<Ability> abilities = new();
 
         public delegate void OnNewAbilityAddedDelegate(Ability ability);
+        public delegate void OnAbilityChangeDelegate(float value, float maxValue);
         public event OnNewAbilityAddedDelegate OnNewAbilityAdded;
+        public event OnAbilityChangeDelegate OnAbilityChange;
+
+        public float Stamina => stamina;
 
         private void Start()
         {
             foreach (Ability ability in initialAbilities)
-            {
                 GiveAbility(ability);
-            }
         }
 
         void GiveAbility(Ability ability)
@@ -25,6 +30,23 @@ namespace Characters.AbilitySystem
             newAbility.Init(this);
             abilities.Add(ability);
             OnNewAbilityAdded?.Invoke(newAbility);
+        }
+
+        public void ActivateAbility(Ability ability)
+        {
+            if (abilities.Contains(ability))
+            {
+                ability.Activate();
+            }
+        }
+
+        public bool TryConsumeStamina(float value)
+        {
+            if (stamina <= value) return false;
+
+            stamina -= value;
+            OnAbilityChange?.Invoke(stamina, maxStamina);
+            return true;
         }
     }
 }
