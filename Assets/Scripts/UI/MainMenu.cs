7940e774b553ee6f549897b275a4a9355f2b777a
DESKTOP-K3JDAAT\Walera <aluschta17@gmail.com>
2023-04-17 06:25:24 +0300

    94 Implement the Main Menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using Level;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button startButton;
+        [SerializeField] LevelManager levelManager;
+
+        private void Start()
+        {
+            startButton.onClick.AddListener(StartGame);
+        }
+
+        private void StartGame()
+        {
+            levelManager.LoadFirstLevel();
+        }
+    }
+}
